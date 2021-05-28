b2b8da17a9cdfcff4fdbae7b109f5076e3b6ac43
Aman30586 <84820873+Aman30586@users.noreply.github.com>
2021-05-28 15:44:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ window.addEventListener('load', () => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const proxy = `http://cors-anywhere.herokuapp.com/`;
-            const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=b86fd8f7708187a762619d1377a88ec7`;
+//             const proxy = `http://cors-anywhere.herokuapp.com/`;
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=b86fd8f7708187a762619d1377a88ec7`;
 
             fetch(api)
                 .then(response => {
